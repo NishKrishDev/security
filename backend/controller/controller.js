d456aabce7eaac983fbc4705a6dba8eb806c7c04
NishantKrishnaDev <nishant@gili.pro>
2025-01-01 12:21:46 +0530

    using bcrypt for password hashing

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -1,5 +1,6 @@
 import { securityPoolDB } from "../database/dbConnection.js"
 import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const algorithm = 'aes-256-cbc';
 const secretKey = 'abcdefghijklmnopqrstuvwxyz123456';
@@ -49,10 +50,9 @@ export const insertData = async (req, res) => {
         console.log(req.body)
         const { id, name } = req.body;
         const { encryptedData, iv } = encryptData(name);
-        const insertQuery = `INSERT INTO public."testTable" (id, name, iv) VALUES ($1, $2, $3);`
-        const values = [id, encryptedData, iv];
+        const insertQuery = `INSERT INTO public."testTable" (name, iv) VALUES ($1, $2);`
+        const values = [encryptedData, iv];
         const result = await securityPoolDB.query(insertQuery, values);
-        console.log(result)
         res.status(200).json({
             "status": 200,
             "message": 'Inserted data successfully'
@@ -69,40 +69,70 @@ export const insertData = async (req, res) => {
 
 export const fetchData = async (req, res) => {
     try {
-        
+
         const { id } = req.body;
 
         const fetchQuery = `Select name, iv from public."testTable" where id = $1`;
         const values = [id];
         const result = await securityPoolDB.query(fetchQuery, values);
 
-        if(result.rows.length === 0)
-        {
+        if (result.rows.length === 0) {
             return res.status(404).json({
-                "status" : 404,
-                "message" : 'Data not found'
+                "status": 404,
+                "message": 'Data not found'
             })
         }
 
-        const {name : encryptedData, iv} = result.rows[0];
+        const { name: encryptedData, iv } = result.rows[0];
         const decryptedData = decryptData(encryptedData, iv);
 
         res.status(200).json({
-            "status" : 200,
-            "message" : 'Data fetched successfully',
-            "data" : {
-                "id" : id,
-                "data" : decryptedData
+            "status": 200,
+            "message": 'Data fetched successfully',
+            "data": {
+                "id": id,
+                "data": decryptedData
             }
         })
 
     } catch (error) {
         console.log(error.message);
         res.status(400).json({
-            "status" : 400,
-            "error" : error.message,
-            "message" : 'Unable to fetch data from database'
+            "status": 400,
+            "error": error.message,
+            "message": 'Unable to fetch data from database'
         })
     }
 }
 
+
+export const insertPassword = async (req, res) => {
+
+    const { name, password } = req.body;
+    if (!password) {
+        return res.status(400).json({
+            'message': 'Password is required'
+        })
+    }
+
+    try {
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const insertPasswordQuery = `INSERT INTO public."testUser"(name, password) VALUES ($1, $2);`;
+        const values = [name, hashedPassword];
+        const result = await securityPoolDB.query(insertPasswordQuery, values);
+
+        res.status(200).json({
+            'name' : name,
+            'password': hashedPassword
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            'status': 500,
+            'error': error.message,
+            'message': 'Error hashing password',
+        })
+    }
+}
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { fetchData, insertData, securityPoolDBController, testController } from '../controller/controller.js';
+import { fetchData, insertData, insertPassword, securityPoolDBController, testController } from '../controller/controller.js';
 
 const router = express.Router();
 
 router.get('/test', testController);
 router.get('/dbTest', securityPoolDBController);
 router.post('/insertData', insertData);
-router.post('/fetchData', fetchData)
+router.post('/fetchData', fetchData);
+router.post('/insertPassword', insertPassword)
 
 export default router;
